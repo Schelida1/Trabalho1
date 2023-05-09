2ab225ad7fc4bc4380632b092a5526580c58ca5a
Schelida Lamusse <s.lamusse@escolar.ifrn.edu.br>
2023-05-08 23:31:45 -0300

    Commit03

--- Trabalho1/Assets/Scripts/Player.cs
+++ Trabalho1/Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rig; 
+    public float velonave; // Essa variavel diz qual a velocidade da nave
+
+    private Vector2 comand; //pega as teclas apertadas 
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update é uma vez por freime 
+    void Update() 
     {
-        
+        movplayer();
+    }
+
+    private void movplayer()
+    {
+        comand = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rig.velocity = comand.normalized * velonave;
     }
 }
